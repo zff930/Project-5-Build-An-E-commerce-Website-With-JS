e7987f65062bb96043bc473d4bb387cf1675ecf6
Feifei Zhang <zwz108@gmail.com>
2025-06-01 10:29:42 -0700

    Created cart.js in the js folder.

---
+++ front/js/cart.js
@@ -0,0 +1,13 @@
+// fetch('http://localhost:3000/api/products/order', {
+//   method: 'POST',
+//   headers: {'Content-Type': 'application/json'},
+//   body: JSON.stringify({firstName, lastName})
+// })
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
+
+console.log(localStorage.getItem('cart'));
