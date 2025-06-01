ebff844a2e616b9a5896b342174768362a177d2e
Feifei Zhang <zwz108@gmail.com>
2025-06-01 11:24:43 -0700

    Commented each step.

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,6 @@
 //Collect the ID of the product that was selected on the homepage.
 const params = new URLSearchParams(window.location.search); // Get the full query string from the URL
-const productId = params.get('id'); // Extract the value of 'id'
+const productId = params.get("id"); // Extract the value of 'id'
 
 //Insert the product and its details into the product page.
 fetch(`http://localhost:3000/api/products/${productId}`)
@@ -12,18 +12,18 @@ fetch(`http://localhost:3000/api/products/${productId}`)
   });
 
 // Get the existing elements on the page where to insert its product info
-const itemImage = document.querySelector('.item__img');
-const itemTitle = document.getElementById('title');
-const itemPrice = document.getElementById('price');
-const itemDescription = document.getElementById('description');
-const itemColors = document.getElementById('colors');
-const itemQuant = document.getElementById('quantity');
-const addToCart = document.getElementById('addToCart');
+const itemImage = document.querySelector(".item__img");
+const itemTitle = document.getElementById("title");
+const itemPrice = document.getElementById("price");
+const itemDescription = document.getElementById("description");
+const itemColors = document.getElementById("colors");
+const itemQuant = document.getElementById("quantity");
+const addToCart = document.getElementById("addToCart");
 
 // Use dynamic info in the string for innerHTML via ${}
 /**
  * Insert the clicked product into its corresponding product page
- * 
+ *
  * @param {object} product - an object that has product info
  */
 function insertProduct(product) {
@@ -42,25 +42,32 @@ function insertProduct(product) {
 // itemQuant.addEventListener('input', () => {
 // });
 
-addToCart.addEventListener('click', () => {
+// Add an EventListener for the "Added to Cart" button
+// so that products will be added to the cart in localStorage
+addToCart.addEventListener("click", () => {
   const product = {
     id: productId,
-    // imageUrl: document.querySelector('.item__img img').getAttribute('src'),
-    // title: itemTitle.textContent,
-    // price: itemPrice.textContent,
+    imageUrl: document.querySelector(".item__img img").getAttribute("src"),
+    title: itemTitle.textContent,
+    price: itemPrice.textContent,
     color: itemColors.value,
-    quantity: parseInt(itemQuant.value)
+    quantity: parseInt(itemQuant.value) // option value is a string, parseInt() converts a string to an int 
   };
 
-  const cart = JSON.parse(localStorage.getItem('cart')) || [];
-  const index = cart.findIndex((item) => {return item.id === product.id && item.color === product.color});
+  const cart = JSON.parse(localStorage.getItem("cart")) || []; // cart can be empty
+
+  // Find the index of the existing product by its id and color
+  const index = cart.findIndex((item) => {
+    return item.id === product.id && item.color === product.color;
+  });
+  // If it exists, update the product quantity
   if (index !== -1) {
     cart[index].quantity += product.quantity;
-  } else {
+  } else { // if not, push the product to the cart
     cart.push(product);
   }
 
-  localStorage.setItem('cart', JSON.stringify(cart));
+  localStorage.setItem("cart", JSON.stringify(cart));
 
-  alert('Product added to cart!');
+  alert("Product added to cart!");
 });
