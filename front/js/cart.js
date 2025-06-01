39d360ab5a87e63617ad9ccde1461878f2ee49e5
Feifei Zhang <zwz108@gmail.com>
2025-06-01 11:27:35 -0700

    Added for loop to insert cart items into DOM.

--- front/js/cart.js
+++ front/js/cart.js
@@ -10,4 +10,35 @@
 //     console.log(data);
 //   });
 
-console.log(localStorage.getItem('cart'));
+const cart = JSON.parse(localStorage.getItem("cart"));
+console.log(cart);
+
+const cartItems = document.getElementById("cart__items");
+
+cart.forEach((item) => {
+  cartItems.innerHTML += `
+    <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+        <div class="cart__item__img">
+            <img src="${item.imageUrl}" alt="Photo of a sofa">
+        </div>
+        <div class="cart__item__content">
+            <div class="cart__item__content__description">
+                <h2>${item.title}</h2>
+                <p>${item.color}</p>
+                <p>${item.price}</p>
+            </div>                  
+            <div class="cart__item__content__settings">
+                <div class="cart__item__content__settings__quantity">
+                    <p>Quantity : </p>
+                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}">
+                </div>
+                <div class="cart__item__content__settings__delete">
+                    <p class="deleteItem">Delete</p>
+                </div>
+            </div>
+        </div>
+    </article>
+  `;
+});
+
+// console.log(cartItems.getAttribute('data-id'));
