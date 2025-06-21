deea8eec0f19663d9375e8584f93739bc14da8a8
Feifei Zhang <zwz108@gmail.com>
2025-06-21 14:01:00 -0700

    Fix the issue that it's allowed to add product of quantity 0 to the cart. And also added product.altTxt to the product details inserted to the page.

--- front/js/product.js
+++ front/js/product.js
@@ -4,17 +4,17 @@ const productId = params.get("id"); // Extract the value of 'id'
 
 //Insert the product and its details into the product page.
 fetch(`http://localhost:3000/api/products/${productId}`)
-  .then(data => data.json())
-  .then(product => insertProduct(product));
+  .then((data) => data.json())
+  .then((product) => insertProduct(product));
 
 // Get the existing elements on the page where to insert its product info
-const itemImage = document.querySelector('.item__img');
-const itemTitle = document.getElementById('title');
-const itemPrice = document.getElementById('price');
-const itemDescription = document.getElementById('description');
-const itemColors = document.getElementById('colors');
-const itemQuant = document.getElementById('quantity');
-const addToCart = document.getElementById('addToCart');
+const itemImage = document.querySelector(".item__img");
+const itemTitle = document.getElementById("title");
+const itemPrice = document.getElementById("price");
+const itemDescription = document.getElementById("description");
+const itemColors = document.getElementById("colors");
+const itemQuant = document.getElementById("quantity");
+const addToCart = document.getElementById("addToCart");
 
 /**
  * Insert the clicked product into its corresponding product page. Use dynamic info in the string for innerHTML via ${}.
@@ -22,7 +22,7 @@ const addToCart = document.getElementById('addToCart');
  */
 function insertProduct(product) {
   // Insert product info into webpage using JS DOM manipulation
-  itemImage.innerHTML = `<img src='${product.imageUrl}' alt='Photo of a sofa'>`;
+  itemImage.innerHTML = `<img src='${product.imageUrl}' alt='${product.altTxt}'>`;
   itemTitle.innerHTML = `${product.name}`;
   itemPrice.innerHTML = `${product.price}`;
   itemDescription.innerHTML = `${product.description}`;
@@ -33,7 +33,7 @@ function insertProduct(product) {
 
 // Add an EventListener for the "Added to Cart" button
 // so that products will be added to the cart in localStorage
-addToCart.addEventListener('click', () => {
+addToCart.addEventListener("click", () => {
   const product = {
     id: productId,
     // imageUrl: document.querySelector('.item__img img').getAttribute('src'),
@@ -43,21 +43,25 @@ addToCart.addEventListener('click', () => {
     quantity: parseInt(itemQuant.value), // option value is a string, parseInt() converts a string to an int
   };
 
-  const cart = JSON.parse(localStorage.getItem('cart')) || []; // cart can be empty
+  const cart = JSON.parse(localStorage.getItem("cart")) || []; // cart can be empty
 
-  // Find the index of the existing product by its id and color
-  const index = cart.findIndex((item) => {
-    return item.id === product.id && item.color === product.color;
-  });
-  // If it exists, update the product quantity
-  if (index !== -1) {
-    cart[index].quantity += product.quantity;
+  if ((product.quantity == 0)) {
+    return;
   } else {
-    // if not, push the product to the cart
-    cart.push(product);
+    // Find the index of the existing product by its id and color
+    const index = cart.findIndex((item) => {
+      return item.id === product.id && item.color === product.color;
+    });
+    // If it exists, update the product quantity
+    if (index !== -1) {
+      cart[index].quantity += product.quantity;
+    } else {
+      // if not, push the product to the cart
+      cart.push(product);
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    alert("Product added to cart!");
   }
-
-  localStorage.setItem('cart', JSON.stringify(cart));
-
-  alert('Product added to cart!');
 });
