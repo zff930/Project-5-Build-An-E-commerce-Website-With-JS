973072c5d1306d18480e462fc3165127d6d39f6b
Feifei Zhang <zwz108@gmail.com>
2025-06-27 01:08:25 -0700

    Updated JSON.parse() so that the parenthesis ends after empty string of array.

--- front/js/product.js
+++ front/js/product.js
@@ -43,7 +43,7 @@ addToCart.addEventListener("click", () => {
     quantity: parseInt(itemQuant.value), // option value is a string, parseInt() converts a string to an int
   };
 
-  const cart = JSON.parse(localStorage.getItem("cart")) || "[]"; // cart can be empty
+  const cart = JSON.parse(localStorage.getItem("cart") || "[]"); // cart can be empty
 
   if (product.quantity == 0) {
     alert("Please add an item!");
