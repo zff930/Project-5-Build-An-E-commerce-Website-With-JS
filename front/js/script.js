ac40b04c4c0ea226963e3cf737e175b6de285568
Feifei Zhang <zwz108@gmail.com>
2025-06-27 00:57:06 -0700

    Updated function document on the function insertProducts(products).

--- front/js/script.js
+++ front/js/script.js
@@ -6,9 +6,8 @@ fetch('http://localhost:3000/api/products')
 // Get the existing element on the page where to insert cards
 const items = document.getElementById('items');
 
-// Use dynamic info in the string for innerHTML via ${}
 /**
- * Insert products into the homepage
+ * Insert products into the homepage. Use dynamic info in the string for innerHTML via ${}.
  *
  * @param {[object]} products - an array of objects that have product info
  */
